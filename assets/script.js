d1e80635774e15c7a5c62be10252b297c02d1984
ColeHeintzman <heintzmancoleanthony@gmail.com>
2022-02-01 17:32:14 -0600

    fixed API and have it loading to page

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,51 @@
-var dateFormEl = document.querySelector("#city-form");
+var dateFormEl = document.querySelector("#input-form");
+var getSome = document.querySelector("#get-some");
+var dateInput = document.querySelector("#searched-date");
+var historyContainer = document.querySelector("#location-date-weather")
+// var example = function (date) {
+//   console.log(date)
+//   getHistoricalEvents(date)
+// }
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var dateVal = dateInput.value;
+  console.log(dateVal);
+  if (dateVal) {
+    //console.log(queryCurrentURL);
+    //example(dateVal);
+    getHistoricalEvents(dateVal);
+    dateInput.value = "";
+  } else {
+    alert("Please enter a Valid Date")
+  }
+}
+
+var createHistory = function (textYear) {
+  for (var i = 0; i < 5; i++) {
+    var h4 = document.createElement("h4");
+    var p = document.createElement("p");
+    h4.textContent = textYear.events[i].text
+    p.textContent = textYear.events[i].year
+    historyContainer.appendChild(h4)
+    historyContainer.appendChild(p)
 
 
-var formSubmitHandler = function(event) {
-    event.preventDefault();
-    var date = document.querySelector("#search-date").value;
-    console.log(date);
-    if (date) {
-      //console.log(queryCurrentURL);
-      getHistoricalEvents(date);
-      inputFormEl.value="";
-    }else{
-      alert("Please enter a Valid Date")
-    }
   }
 
- var getHistoricalEvents = function (date) {
-    var historyURL = "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/" + date;
-    fetch(historyURL).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data)
-        });
-    
-    });
 }
- getHistoricalEvents();
+
+var getHistoricalEvents = function (date) {
+  var historyURL = "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/" + date;
+  console.log(historyURL)
+  fetch(historyURL).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data)
+      createHistory(data);
+      data.events[0].text
+    });
+
+  });
+}
+//getHistoricalEvents();
+getSome.addEventListener("click", formSubmitHandler);
